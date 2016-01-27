102766c8d6fb81a221945abe3f65963822f70bf9
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-01-27 19:18:11 +0530

    changes the shortestPath algo

--- tests/weightedGraphsTest.js
+++ tests/weightedGraphsTest.js
@@ -3,14 +3,13 @@ var assert = require('chai').assert;
 var ld = require('lodash');
 
 
-describe("shortest path", function() {
+describe("shortest path", function(done) {
     it("should choose the only path when only one path exists", function() {
         var g = new graphs.WeightedGraph();
         g.addVertex('A');
         g.addVertex('B');
         var e1 = new graphs.Edge("e1", 'A', 'B', 1);
         g.addEdge(e1);
-
         var path = g.shortestPath('A', 'B');
         assert.equal(1, path.length);
         assert.deepEqual(e1, path[0]);
@@ -46,7 +45,6 @@ describe("shortest path", function() {
         g.addEdge(e1);
         g.addEdge(e2);
         g.addEdge(e3);
-
         var path = g.shortestPath('A', 'C');
         assert.equal(2, path.length);
         assert.deepEqual(e1, path[0]);
@@ -58,14 +56,52 @@ describe("shortest path", function() {
         g.addVertex('A');
         g.addVertex('B');
 
-        var e1 = new graphs.Edge("e1", 'A', 'B', 1);
+        var e1 = new graphs.Edge("e1", 'A', 'B', 3);
         var e2 = new graphs.Edge("e2", 'A', 'B', 2);
         g.addEdge(e1);
         g.addEdge(e2);
 
         var path = g.shortestPath('A', 'B');
         assert.equal(1, path.length);
-        assert.deepEqual(e1, path[0]);
+        assert.deepEqual(e2, path[0]);
+    });
+
+    it("should works for dense graph", function() {
+        var g = denseGraph();
+        var extra1 = new graphs.Edge('extra1', 'A', 'C', 1);
+        var extra2 = new graphs.Edge('extra2', 'C', 'B', 1);
+        var e1 = g.addEdge(extra1);
+        var e2 = g.addEdge(extra2);
+       
+        var path = g.shortestPath('A', 'B');
+        assert.equal(2, path.length);
+        assert.deepEqual(extra1, path[0]);
+        assert.deepEqual(extra2, path[1]);
     });
 
 });
+
+function denseGraph() {
+    var g = new graphs.WeightedGraph();
+    var vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var s = {};
+
+    vertices.forEach(function(vertex) {
+        g.addVertex(vertex);
+    });
+
+    var count = 1;
+    for (var i = 0; i < vertices.length - 1; i++) {
+        var from = vertices[i];
+        for (var j = 0; j < vertices.length; j++) {
+            if (vertices[j] != vertices[i]) {
+                s['e' + count] = new graphs.Edge('e' + count, from, vertices[j], 5);
+                count++;
+            };
+        }
+    }
+    for (var edg in s) {
+        g.addEdge(s[edg]);
+    }
+    return g;
+}
